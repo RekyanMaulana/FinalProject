d93574320173afcfb09f61bff43a99592d5a8185
= <=>
2023-05-23 16:27:05 +0700

    template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiswaController; //panggil controller yang ada dibuat sebelumnya
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DivisiController;
+use App\Http\Controllers\GajiController;
+use App\Http\Controllers\JabatanController;
+use App\Http\Controllers\MateriController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\PelatihanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +24,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/salam', function () {
+    return "Selamat pagi";
+}); // ini adalah routing untuk pemanggilan dirinya sendiri
+Route::get('/ucapan', function () {
+    return view('ucapan'); //ini adalah routing yang mengarahkan ke view yang ada di folder 
+    //resources/views
+});
+Route::get('/nilai', function () {
+    return view('nilai');
+}); //arahkan return nilai ke file nilai yang ada di view 
+Route::get('/daftar_nilai', function () {
+    return view('daftar_nilai');
+});
+//mengarahkan routing ke controller
+Route::get('/siswa', [SiswaController::class, 'dataSiswa']);
+//mengarahkan ke controller dashboardController
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('index');
+
+// bisa make ini 
+Route::resource('pegawai', PegawaiController::class);
+
+// atau ini bebas dah
+// Route::get('/pegawai', [PegawaiController::class, 'index']);

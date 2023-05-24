8c67f6bcc5d1d2a9414a059d16528df7c643c135
RekyanMaulana <rekyan.maulana@gmail.com>
2023-05-24 15:33:36 +0700

    upload final project

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SiswaController; //panggil controller yang ada dibuat sebelumnya
-use App\Http\Controllers\DashboardController;
+
 use App\Http\Controllers\DetailTransaksiController;
-use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\UserController;
 
@@ -28,32 +26,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/salam', function () {
-    return "Selamat pagi";
-}); // ini adalah routing untuk pemanggilan dirinya sendiri
-Route::get('/ucapan', function () {
-    return view('ucapan'); //ini adalah routing yang mengarahkan ke view yang ada di folder 
-    //resources/views
-});
-Route::get('/nilai', function () {
-    return view('nilai');
-}); //arahkan return nilai ke file nilai yang ada di view 
-Route::get('/daftar_nilai', function () {
-    return view('daftar_nilai');
-});
-//mengarahkan routing ke controller
-Route::get('/siswa', [SiswaController::class, 'dataSiswa']);
-//mengarahkan ke controller dashboardController
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('index');
-
 // bisa make ini 
 Route::resource('transaksi', TransaksiController::class);
 Route::resource('detail_transaksi', DetailTransaksiController::class);
 Route::resource('user', UserController::class);
-
-// atau ini bebas dah
-Route::get('/pegawai', [PegawaiController::class, 'index']);
-
 
 Route::get('/penjual', [PenjualController::class, 'index']);
 Route::get('/product', [ProductController::class, 'index']);
